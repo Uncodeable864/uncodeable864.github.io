b8a63e170a75b1db933b69d8fc16ca945b4db669
Uncodeable864 <49599682+Uncodeable864@users.noreply.github.com>
2022-04-13 19:19:24 -0500

    Added home page

---
+++ pages/index.tsx
@@ -0,0 +1,40 @@
+import { Edit2, Smartphone, Youtube } from "lucide-react";
+import Head from "next/head";
+import styles from "../styles/Home.module.scss";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Uncodeable864</title>
+        <meta name="description" content="A personal website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>Uncodeable864</h1>
+        <p className={styles.subtitle}>A collection of stuff I've done</p>
+        <div className={styles.callToAction}>
+          <a href="/posts">
+            <button>
+              <Edit2 />
+              <p>BLOG</p>
+            </button>
+          </a>
+          <a href="/apps">
+            <button>
+              <Smartphone />
+              <p>APPS</p>
+            </button>
+          </a>
+          <a href="https://tinyurl.com/alphaUncodeable">
+            <button>
+              <Youtube />
+              <p>YOUTUBE</p>
+            </button>
+          </a>
+        </div>
+      </main>
+    </div>
+  );
+}
